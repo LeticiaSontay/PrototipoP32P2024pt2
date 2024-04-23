53c5dde9b3a1ba65e1f8caf2ed86983075b7346b
Leticia Sontay <lsontayc1@miumg.edu.gt>
2024-04-22 23:57:50 -0600

    agregado controlador peliculas y PeliculasDAO

--- src/main/java/controlador/Peliculas.java
+++ src/main/java/controlador/Peliculas.java
@@ -25,11 +25,51 @@ public class Peliculas {
         return "Peliculas{" + "nombre=" + nombre + ", clasificacion=" + clasificacion + ", subtitulado=" + subtitulado + "idioma=" + idioma + ", tipo=" + tipo + ", precio=" + precio + '}';
     }
 
-    public String getnombre() {
-        return nombe;
+    public String getNombre() {
+        return nombre;
     }
 
-    public void setCodigo_curso(String codigo_curso) {
-        this.codigo_curso = codigo_curso;
-    } 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getSubtitulado() {
+        return subtitulado;
+    }
+
+    public void setSubtitulado(String subtitulado) {
+        this.subtitulado = subtitulado;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
 }
--- src/main/java/modelo/PeliculasDAO.java
+++ src/main/java/modelo/PeliculasDAO.java
@@ -4,10 +4,30 @@
  */
 package modelo;
 
+import controlador.Peliculas;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
 /**
  *
  * @author alumno
  */
 public class PeliculasDAO {
+    
+    private static final String SQL_SELECT = "SELECT nombre, clasificacion, subtitulado, idioma, tipo, precio FROM peliculas";
+    private static final String SQL_INSERT = "INSERT INTO peliculas(nombre, clasificacion, subtitulado, idioma, tipo, precio) VALUES(?, ?, ?, ?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE peliculas SET clasificacion=?, subtitulado=?, idioma=?, tipo=?, precio=? WHERE nombre=?";
+    private static final String SQL_DELETE = "DELETE FROM peliculas WHERE nombre=?";
+    private static final String SQL_QUERY = "SELECT nombre, clasificacion, subtitulado, idioma, tipo, precio FROM peliculas WHERE nombre = ?";
+    
+    
+    
     
 }
